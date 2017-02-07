669b7f251e4dd1385326777851bc271a6f7aeff3
TimothyRatliff <timothykratliff@gmail.com>
2017-02-07 03:49:37 -0600

    gateenabletrigger complete

---
+++ Assets/gateenabletrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gateenabletrigger : MonoBehaviour
+{
+    public GameObject ball;
+    public GameObject gate;
+
+    void Start ()
+    {
+        if (ball == null)
+            ball = GameObject.FindWithTag("ball");
+        if (gate == null)
+            gate = GameObject.FindWithTag("gate");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "ball")
+        {
+            Debug.Log("Gate closed");
+            gate.SetActive(true);
+        }
+    }
+}
