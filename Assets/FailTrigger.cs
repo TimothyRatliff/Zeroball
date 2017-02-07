af1200ddaa629bdaa064f0b19b5223887979c0b0
TimothyRatliff <timothykratliff@gmail.com>
2017-02-06 23:13:32 -0600

    work on failtrigger

--- Assets/FailTrigger.cs
+++ Assets/FailTrigger.cs
@@ -3,9 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FailTrigger : MonoBehaviour {
+    Vector2 position;
+
+    void Start ()
+    {
+        position = new Vector2(4.604, -3.959);
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        //other.gameObject.GetComponent<>
+        if (other.gameObject.tag == "ball")
+        {
+            Debug.Log("Reset ball posiion to start");
+            other.transform.position = position;
+            //other.transform.rotation = Vecotr3(0, 0, 0);
+        }
     }
 }
