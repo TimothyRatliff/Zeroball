039eab163802f7cb67598b5b0e1b95bf896b753c
TimothyRatliff <timothykratliff@gmail.com>
2017-02-07 03:26:38 -0600

    add rflapmotor and start replication
    also remove platformR

--- Assets/flapmotor.cs
+++ Assets/flapmotor.cs
@@ -5,27 +5,41 @@ using UnityEngine;
 public class flapmotor : MonoBehaviour {
     HingeJoint2D[] hingeJoints;
     JointMotor2D jointMotor;
+    Vector2 position;
+    Vector3 rotation;
+    public GameObject lflap;
+
 
     void Start ()
     {
         hingeJoints = gameObject.GetComponents<HingeJoint2D>();
-        jointMotor = hingeJoints[1].motor;
+        jointMotor = hingeJoints[0].motor;
+        if (lflap == null)
+            lflap = GameObject.FindWithTag("lflap");
+        position = lflap.transform.position;
+        rotation = lflap.transform.rotation.eulerAngles;
     }
 
 	void Update ()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    jointMotor.motorSpeed = -1000;
+        //    hingeJoints[0].motor = jointMotor;
+        //}
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             jointMotor.motorSpeed = -1000;
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            jointMotor.motorSpeed = 1000;
+            hingeJoints[0].motor = jointMotor;
         }
         else
         {
             jointMotor.motorSpeed = 0;
+            hingeJoints[0].motor = jointMotor;
+            Debug.Log("Reset left paddle to standard");
+            lflap.transform.position = position;
+            rotation.z = -15.876f;
+            lflap.transform.rotation = Quaternion.Euler(rotation);
         }
-        hingeJoints[1].motor = jointMotor;
     }
 }
---
+++ Assets/rflapmotor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rflapmotor : MonoBehaviour {
+    HingeJoint2D[] hingeJoints;
+    JointMotor2D jointMotor;
+    Vector2 position;
+    Vector3 rotation;
+    public GameObject rflap;
+
+
+    void Start()
+    {
+        hingeJoints = gameObject.GetComponents<HingeJoint2D>();
+        jointMotor = hingeJoints[0].motor;
+        if (rflap == null)
+            rflap = GameObject.FindWithTag("rflap");
+        position = rflap.transform.position;
+        rotation = rflap.transform.rotation.eulerAngles;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            jointMotor.motorSpeed = 1000;
+            hingeJoints[0].motor = jointMotor;
+        }
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    jointMotor.motorSpeed = -1000;
+        //    hingeJoints[0].motor = jointMotor;
+        //}
+        else
+        {
+            jointMotor.motorSpeed = 0;
+            hingeJoints[0].motor = jointMotor;
+            Debug.Log("Reset right paddle to standard");
+            rflap.transform.position = position;
+            rotation.z = -152.411f;
+            rflap.transform.rotation = Quaternion.Euler(rotation);
+        }
+    }
+}
