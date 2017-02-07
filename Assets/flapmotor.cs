cdf8bfc3bd28c47e9dc8b2f5e8561e09a9deabfc
TimothyRatliff <timothykratliff@gmail.com>
2017-02-07 02:42:21 -0600

    edit flapmotor

--- Assets/flapmotor.cs
+++ Assets/flapmotor.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class flapmotor : MonoBehaviour {
+    HingeJoint2D[] hingeJoints;
+    JointMotor2D jointMotor;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start ()
+    {
+        hingeJoints = gameObject.GetComponents<HingeJoint2D>();
+        jointMotor = hingeJoints[1].motor;
+    }
+
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            jointMotor.motorSpeed = -1000;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            jointMotor.motorSpeed = 1000;
+        }
+        else
+        {
+            jointMotor.motorSpeed = 0;
+        }
+        hingeJoints[1].motor = jointMotor;
+    }
 }
