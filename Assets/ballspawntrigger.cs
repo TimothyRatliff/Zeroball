be917999334fa1f3805848d0e689e6a817fc29ba
TimothyRatliff <timothykratliff@gmail.com>
2017-02-07 03:46:07 -0600

    create ballspawntrigger, and edit some tags
    also create gateenabletrigger gameObject

---
+++ Assets/ballspawntrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballspawntrigger : MonoBehaviour {
+    public GameObject ball;
+    public GameObject gate;
+
+    void Start()
+    {
+        if (ball == null)
+            ball = GameObject.FindWithTag("ball");
+        if (gate == null)
+            gate = GameObject.FindWithTag("gate");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "ball")
+        {
+            Debug.Log("Ball has spawned, open gate");
+            gate.SetActive(false);
+        }
+    }
+
+}
